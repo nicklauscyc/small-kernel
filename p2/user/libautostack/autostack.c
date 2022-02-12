b46b1884d82d91d679b1221db68a0c924dd592af
Nicklaus Choo <44658711+nicklauscyc@users.noreply.github.com>
2022-02-12 12:37:27 -0500

    autostack with swe_interrupt_handler

--- p2/user/libautostack/autostack.c
+++ p2/user/libautostack/autostack.c
@@ -3,11 +3,53 @@
  * instead.
  */
 
-void install_autostack(void * stack_high, void * stack_low);
+#include <ureg.h>
+#include <syscall.h> /* PAGE_SIZE */
+#include <simics.h> /* lprintf() */
+#include <stdint.h>  /* uint32_t */
 
+/* intel-sys.pdf pg182 lowest bit P=0 if fault by non-present page, 1 o/w */
+#define ERR_P_PROTECTION_VIOLATION_MASK 0x00000001
+
+
+void install_autostack(void * stack_high, void * stack_low);
+void pf_swexn_handler(void *arg, ureg_t *ureg);
+
+/* TODO I guess this guy installs the handler? */
 void
 install_autostack(void *stack_high, void *stack_low)
 {
+	lprintf("install_autostack() stack_high: %p, stack_low: %p\n",
+	        stack_high, stack_low);
   (void)stack_high;
   (void)stack_low;
 }
+
+/** @brief Page fault exception handler
+ */
+void pf_swexn_handler(void *arg, ureg_t *ureg)
+{
+	/* Get relevant info */
+	int cause = ureg->cause;
+
+	/* Memory address resulting in fault */
+	unsigned int cr2 = ureg->cr2;
+
+	/* Why memory address was inaccessible */
+	unsigned int error_code = ureg->error_code;
+
+	// TODO below not needed?
+	// unsigned int eip = ureg->eip;
+	//
+	lprintf("pf_swexn_handler() cause: %x, cr2: %x, error_code: %x\n",
+	        cause, cr2, error_code);
+
+	/* Only deal with pagefault execptions */
+	if (cause != SWEXN_CAUSE_PAGEFAULT) return;
+
+	/* Only deal with faults caused by non-present page */
+	if (!(ERR_P_PROTECTION_VIOLATION_MASK & error_code)) {
+		new_pages((void *) (uint32_t) cr2, PAGE_SIZE);
+	}
+}
+
