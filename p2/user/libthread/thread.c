cae45e68356a7dc5f4d22541b7a408a67d210f23
Nicklaus Choo <44658711+nicklauscyc@users.noreply.github.com>
2022-02-13 17:45:20 -0500

    empty thr_init() function

---
+++ p2/user/libthread/thread.c
@@ -0,0 +1,14 @@
+/** @file thread.c
+ *
+ *  @brief This file implements the Thread Management API declared in
+ *         thread.h
+ *
+ *  @author Nicklaus Choo (nchoo)
+ */
+
+int
+thr_init(unsigned int size)
+{
+	return 0;
+}
+
