fd56f967d2fac8098d83bd916a22aeaa070200e6
Nicklaus Choo <44658711+nicklauscyc@users.noreply.github.com>
2022-02-21 18:52:17 -0500

    panic function done

--- p2/user/libthread/thr_internals.c
+++ p2/user/libthread/thr_internals.c
@@ -2,6 +2,8 @@
  *	@brief Helper functions for thread library internals
  *	@author Nicklaus Choo (nchoo)
  */
+
+#include <stdarg.h> /* va_list(), va_end() */
 #include <simics.h> /* sim_puts() */
 #include <syscall.h> /* gettid() */
 #include <stdio.h> /* snprintf(), vsnprintf() */
@@ -9,6 +11,8 @@
 #define LEN 256
 
 /** @brief Prepends the thread id to printed format
+ *
+ *  Observe that va_end() is used after va_start() to prevent stack corruption.
  *
  *  @param format String format to print to
  *  @param ... All other arguments for string format
@@ -27,6 +31,7 @@ tprintf(const char *format, ... )
 	va_start(args, format);
 	vsnprintf(str + offset, sizeof(str) - 1, format, args);
 	sim_puts(str);
+	va_end(args);
 
 	return;
 }
