b16a618ceb3ad88b4a5de3d613ca6bd680c56669
Andre <cnasc.andre@gmaill.com>
2022-02-21 22:34:22 -0500

    Passign rw lock test

--- p2/user/inc/rwlock_type.h
+++ p2/user/inc/rwlock_type.h
@@ -8,18 +8,17 @@
 #include <mutex_type.h>
 #include <cond_type.h>
 
+/** @brief Enumerator for possible states of a r/w lock */
 typedef enum { READING, WRITING, NONE } state_t;
 
-typedef struct thr_node {
-    int tid;
-    struct thr_node *next;
-} thr_node_t;
-
+/** @brief Struct containing all info for the read/write lock */
 typedef struct rwlock {
     state_t state;      /** Current state of the lock */
     cond_t readers;     /** Queue of threads waiting to read */
     cond_t writers;     /** Queue of threads waiting to write */
     int num_active;     /** List of active lock holders */
+    int num_waiting_readers; /** Number of readers waiting in queue */
+    int num_waiting_writers; /** Number of writers waiting in queue */
     int initialized;    /** Whether this lock has been initialized */
     mutex_t state_mux;  /** Mutex for modifying rwlock state */
 } rwlock_t;
--- p2/user/libthread/rwlock.c
+++ p2/user/libthread/rwlock.c
@@ -1,13 +1,51 @@
 /** @file rwlock.c
  *  @brief A read/write lock
  *
- *  - Talk about design (coalescing readers)
+ *  Design:
+ *  A read/write lock must allow either one writer or multiple readers
+ *  at a time. The approach here is to try and serve readers or writers
+ *  roughly in the order of arrival. Suppose the following order of requests:
+ *
+ *  W1 R1 R2 W2
+ *
+ *  Then, we could avoid starvation of readers and writers by letting
+ *  W1 go first, then once its done we let R1 R2 go simulateneously, and
+ *  lastly, we let W2 go. However, this simple policy is can be rather
+ *  bad. Suppose the following sequence:
+ *
+ *  W1 R1 R2 W2 R3 R4
+ *
+ *  Then, if we had known of requests R3 and R4 before W1 gave
+ *  up its lock, we could reorganize our serving order as:
+ *
+ *  W1 R1 R2 R3 R4 W2
+ *
+ *  This enables us to serve all 4 readers in parallel. To enable
+ *  this "coalescing" of readers we make the following policy:
+ *
+ *  - Write requests go into a writer queue
+ *  - Read requests go into a reader queue
+ *  - Whenever we transition from WRITING to READING we service *all*
+ *    read requests.
+ *
+ *  This simple policy equates to "coalescing" all reads together.
+ *  Wouldn't this lead to a starvation of writers, though? No. To avoid
+ *  the starvation of writers, we only coalesce when a writer has the lock.
+ *  When we are in the reading phase we accept no new readers.
+ *
+ *  Formulated more simply, we only "coalesce" readers during phase
+ *  transitions, which allows us to avoid starvation while enabling
+ *  high read parallelism.
+ *
+ *
+ *  @author Andre Nascimento (anascime)
  *  */
 
 #include <stdlib.h>
 #include <mutex.h>
 #include <cond.h>
-#include <rwlock_type.h> /* rwlock_t */
+#include <assert.h>
+#include <rwlock.h> /* rwlock_t, RWLOCK_READ, RWLOCK_WRITE */
 
 static void wait_for_turn( rwlock_t *rwlock, state_t state );
 static void start_reader_phase( rwlock_t *rwlock );
@@ -57,7 +95,7 @@ rwlock_lock( rwlock_t *rwlock, int type )
 
     affirm(rwlock->initialized);
 
-    state_t requested_state = type == RWLOCK_READ ? READER : WRITER;
+    state_t requested_state = type == RWLOCK_READ ? READING : WRITING;
 
     if (rwlock->state == NONE) {
         /* First come, first serve */
@@ -65,15 +103,15 @@ rwlock_lock( rwlock_t *rwlock, int type )
         rwlock->num_active++;
         rwlock->state = requested_state;
 
-    } else if (rwlock->state == READER) {
+    } else if (rwlock->state == READING) {
         /* If no writers are waiting, just give out reader locks. */
-        if (requested_state == READER && rwlock->num_waiting_writers == 0) {
+        if (requested_state == READING && rwlock->num_waiting_writers == 0) {
             rwlock->num_active++;
         } else {
             wait_for_turn(rwlock, requested_state);
         }
 
-    } else if (rwlock->state == WRITER) {
+    } else if (rwlock->state == WRITING) {
         wait_for_turn(rwlock, requested_state);
 
     } else {
@@ -99,7 +137,7 @@ void rwlock_unlock( rwlock_t *rwlock )
     // access to the critical section?
     affirm(rwlock->initialized);
 
-    if (rwlock->state == READER) {
+    if (rwlock->state == READING) {
         affirm(rwlock->num_active > 0);
         rwlock->num_active--;
 
@@ -107,11 +145,11 @@ void rwlock_unlock( rwlock_t *rwlock )
         if (rwlock->num_active == 0)
             start_writer_phase(rwlock);
 
-    } else if (rwlock->state == WRITER) {
+    } else if (rwlock->state == WRITING) {
         affirm(rwlock->num_active == 1);
         rwlock->num_active = 0;
 
-        start_reader_phase();
+        start_reader_phase(rwlock);
 
     } else {
         /* NOTREACHED */
@@ -164,15 +202,13 @@ void rwlock_downgrade( rwlock_t *rwlock )
 
     mutex_lock(&rwlock->state_mux);
 
-    // TODO: How can we check that this is the same thread
-    // as the one who currently has write access?
     affirm(rwlock->initialized && rwlock->num_active == 1
-            && rwlock->state == WRITER);
+            && rwlock->state == WRITING);
 
     /* Since this is a downgrade, the caller becomes a reader
      * and all readers are invited to join.  */
     rwlock->num_active = 1;
-    rwlock->state = READER;
+    rwlock->state = READING;
     cond_broadcast(&rwlock->readers);
 
     mutex_unlock(&rwlock->state_mux);
@@ -189,14 +225,14 @@ static void start_writer_phase( rwlock_t *rwlock )
 {
     /* Let a single writer in */
     if (rwlock->num_waiting_writers > 0) {
-        rwlock->state = WRITER;
+        rwlock->state = WRITING;
         cond_signal(&rwlock->writers);
         return;
     }
 
     /* If no writer waiting, let all readers in */
     if (rwlock->num_waiting_readers > 0) {
-        rwlock->state = READER;
+        rwlock->state = READING;
         cond_broadcast(&rwlock->readers);
         return;
     }
@@ -212,14 +248,14 @@ static void start_reader_phase( rwlock_t *rwlock )
 {
     /* Let all waiting readers in */
     if (rwlock->num_waiting_readers > 0) {
-        rwlock->state = READER;
+        rwlock->state = READING;
         cond_broadcast(&rwlock->readers);
         return;
     }
 
     /* However, if no one's waiting to read let writers have a go */
     if (rwlock->num_waiting_writers > 0) {
-        rwlock->state = WRITER;
+        rwlock->state = WRITING;
         cond_signal(&rwlock->writers);
         return;
     }
@@ -248,10 +284,10 @@ static void wait_reader( rwlock_t *rwlock )
 {
     rwlock->num_waiting_readers++;
 
-    /* Wait until next READER state. This can be triggered early
+    /* Wait until next READING state. This can be triggered early
      * if readers is signaled before a writer has had a chance to
      * lock. Since we expect this to not occurr, this check suffices. */
-    while (rwlock->state != READER)
+    while (rwlock->state != READING)
         cond_wait(&rwlock->readers, &rwlock->state_mux);
 
     rwlock->num_waiting_readers--;
@@ -264,9 +300,9 @@ static void wait_reader( rwlock_t *rwlock )
  *  returns, it will be locked. */
 static void wait_for_turn( rwlock_t *rwlock, state_t state )
 {
-    if (state == READER)
+    if (state == READING)
         wait_reader(rwlock);
-    if (state == WRITER)
+    if (state == WRITING)
         wait_writer(rwlock);
 }
 
