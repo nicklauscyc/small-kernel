287b43415404ceefab8f98c7c5e38cc976994702
Nicklaus Choo <44658711+nicklauscyc@users.noreply.github.com>
2022-02-23 12:49:47 -0500

    rwlock_type docs

--- p2/user/inc/rwlock_type.h
+++ p2/user/inc/rwlock_type.h
@@ -11,16 +11,26 @@
 /** @brief Enumerator for possible states of a r/w lock */
 typedef enum { READING, WRITING, NONE } state_t;
 
-/** @brief Struct containing all info for the read/write lock */
+/** @brief Struct containing all info for the read/write lock
+ *
+ *  @param state Current state of the lock
+ *  @param readers Queue of threads waiting to read
+ *  @param writers Queue of threads waiting to write
+ *  @param num_active List of active lock holders
+ *  @param num_waiting_readers Number of readers waiting in queue
+ *  @param num_waiting_writers Number of writers waiting in queue
+ *  @param initialized Whether this lock has been initialized
+ *  @param state_mux Mutex for modifying rwlock state
+ */
 typedef struct rwlock {
-    state_t state;      /** Current state of the lock */
-    cond_t readers;     /** Queue of threads waiting to read */
-    cond_t writers;     /** Queue of threads waiting to write */
-    int num_active;     /** List of active lock holders */
-    int num_waiting_readers; /** Number of readers waiting in queue */
-    int num_waiting_writers; /** Number of writers waiting in queue */
-    int initialized;    /** Whether this lock has been initialized */
-    mutex_t state_mux;  /** Mutex for modifying rwlock state */
+    state_t state;
+    cond_t readers;
+    cond_t writers;
+    int num_active;
+    int num_waiting_readers;
+    int num_waiting_writers;
+    int initialized;
+    mutex_t state_mux;
 } rwlock_t;
 
 #endif /* _RWLOCK_TYPE_H */
