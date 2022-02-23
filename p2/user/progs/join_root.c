deea98890a808e3b87ed6bf0a8dc4ee5c67341ce
drenascimento <cnasc.andre@gmail.com>
2022-02-23 00:07:15 -0300

    Update join_root.c test

--- p2/user/progs/join_root.c
+++ p2/user/progs/join_root.c
@@ -16,12 +16,10 @@ add_one(void *root_tid)
 {
 	tprintf("running child sees root_tid: %d", *((int *) root_tid));
 
-	int x = 0;
-	int *status;
-	*status = x;
-    thr_join(*(int *)root_tid, (void **) &status);
+	void *status;
+    	thr_join(*(int *)root_tid, (void **) &status);
 
-	assert(x == 69);
+	assert(*(int *)status == 69);
 	thr_exit(0);
 	return 0;
 }
@@ -34,8 +32,9 @@ int main()
 	int tid = thr_create(add_one, &root_tid);
 	lprintf("created thread %d", tid);
 
-	int y = 69;
-	thr_exit(&y);
+	int yp = malloc(sizeof(int));
+	*yp = 69;
+	thr_exit(yp);
 
 
 	return 0;
