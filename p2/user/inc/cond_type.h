9a21aca3501b3859126995e851c51df8032dd3b5
Nicklaus Choo <44658711+nicklauscyc@users.noreply.github.com>
2022-02-23 12:39:49 -0500

    document cond_type.h

--- p2/user/inc/cond_type.h
+++ p2/user/inc/cond_type.h
@@ -1,24 +1,38 @@
 /** @file cond_type.h
  *  @brief This file defines the type for condition variables.
+ *
+ *  There are some important warnings for users using the condition variables.
+ *  The most important is to never lock or unlock or interact directly with
+ *  the mutex for condition variables in any way. Please stick to the
+ *  interface of condition variables or else risk undefined behavior.
+ *
+ *  @author Nicklaus Choo (nchoo)
  */
 
 #ifndef _COND_TYPE_H
 #define _COND_TYPE_H
 
 #include <mutex.h> /* mutex_t */
-#include <variable_queue.h>
+#include <variable_queue.h> /* Q_NEW_LINK(), Q_NEW_HEAD() */
 
-/* Declare type of cvar_node_t */
-// TODO not sure where this should go
-/** @brief struct for each node in cv->queue
+/** @brief Struct for each node in condition variable queue
+ *
+ *  On macro expansion, the entire struct looks like this:
  *
  *  typedef struct cvar_node {
  *  	   struct {
  *  	       struct cvar_node *prev;
  *  	       struct cvar_node *next;
- *  	   }
- *  	   thr_status_t *thr_info;
+ *  	   } link;
+ *  	   int tid;
+ *  	   mutex_t *mp;
+ *  	   int descheduled;
  *  } cvar_node_t;
+ *
+ *  @param link Contains pointers to previous and next nodes on the linked list
+ *  @param tid Thread ID of thread waiting on this condition variable
+ *  @param mp Mutex that thread was holding on to on cond_wait() call
+ *  @param descheduled 1 if thread is set to be descheduled, 0 otherwise
  */
 typedef struct cvar_node {
 	Q_NEW_LINK(cvar_node) link;
@@ -27,27 +41,35 @@ typedef struct cvar_node {
 	int descheduled;
 } cvar_node_t;
 
-/* Declare type of cvar_queue_t */
-/*
-typedef struct {
-    struct cvar_node *front;
-    struct cvar_node *tail;
-} cvar_queue_t;
+/** @brief Struct for queue head of the condition variable
+ *
+ * 	On macro expansion, the entire struct looks like this:
+ *
+ *  typedef struct {
+ * 	    struct cvar_node *front;
+ *      struct cvar_node *tail;
+ *  } cvar_queue_t;
+ *
+ *  @param front Front (first element) of queue.
+ *  @param tail Tail (last element) of queue
  */
 Q_NEW_HEAD(cvar_queue_t, cvar_node);
 
-/* WARNING: DO NOT EVER LOCK/UNLOCK/INTERACT WITH THE MUTEX FOR CONDITION
- * VARIABLES DIRECTLY. THE CONDITION VARIABLE MUTEX SHOULD ONLY BE
- * USED BY OTHER cond_ FUNCTIONS. DISOBEYING THIS INTERFACE WILL LEAD TO
- * UNDEFINED BEHAVIOR
+/** @brief Struct for the condition variable.
+ *
+ *  WARNING: DO NOT EVER LOCK/UNLOCK/INTERACT WITH THE MUTEX FOR CONDITION
+ *  VARIABLES DIRECTLY. THE CONDITION VARIABLE MUTEX SHOULD ONLY BE
+ *  USED BY OTHER cond_ FUNCTIONS. DISOBEYING THIS INTERFACE WILL LEAD TO
+ *  UNDEFINED BEHAVIOR.
+ *
+ *  @param mp Mutex that the condition variable uses to guard its queue
+ *  @param qp Pointer to head of the queue of waiting threads
+ *  @param init 1 if condition variable has been initialized, 0 otherwise
  */
-/* Condition variable */
 typedef struct cond {
 	mutex_t *mp;
 	cvar_queue_t *qp;
 	int init;
 } cond_t;
-
-
 
 #endif /* _COND_TYPE_H */
