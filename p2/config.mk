422e8ef38dd903a54665b61f04dd3642889c72d5
Nicklaus Choo <44658711+nicklauscyc@users.noreply.github.com>
2022-02-23 15:13:37 -0500

    divide by zero test

--- p2/config.mk
+++ p2/config.mk
@@ -141,7 +141,8 @@ UPDATE_METHOD = afs
 # A list of the test programs you want compiled in from the user/progs
 # directory
 #
-STUDENTTESTS = test_set_status test_threads test_rwlock childpf join_root
+STUDENTTESTS = test_set_status test_threads test_rwlock childpf join_root \
+               div_zero
 
 ###########################################################################
 # Object files for your thread library
