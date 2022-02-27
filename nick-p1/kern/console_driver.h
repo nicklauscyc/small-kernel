7bd052a26de6167c3a39d1d8674d3955c7e75942
Nicklaus Choo <44658711+nicklauscyc@users.noreply.github.com>
2022-02-27 17:10:16 -0500

    added nick p1 code

---
+++ nick-p1/kern/console_driver.h
@@ -0,0 +1,13 @@
+/** @ file console_driver.h
+ *  @ brief Contains helpful internal functions for other drivers to use.
+ *          Mainly the keyboard driver
+ *
+ *  @author Nicklaus Choo (nchoo)
+ *  @bug No known bugs.
+ */
+#ifndef _P1_CONSOLE_DRIVER_H_
+#define _P1_CONSOLE_DRIVER_H_
+
+int _putbyte(char ch, int *scrolled);
+
+#endif
