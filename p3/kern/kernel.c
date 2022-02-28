abbb24a1f0e9f502e21bc0d2f8575115759a8a8c
Nicklaus Choo <44658711+nicklauscyc@users.noreply.github.com>
2022-02-27 20:11:05 -0500

    just copy pasted nick's console functions for now

--- p3/kern/kernel.c
+++ p3/kern/kernel.c
@@ -21,10 +21,13 @@
 /* x86 specific includes */
 #include <x86/asm.h>                /* enable_interrupts() */
 
+
+#include <console.h> /* clear_console() */
+
 volatile static int __kernel_all_done = 0;
 
 /** @brief Kernel entrypoint.
- *  
+ *
  *  This is the entrypoint for the kernel.
  *
  * @return Does not return
@@ -36,6 +39,8 @@ int kernel_main(mbinfo_t *mbinfo, int argc, char **argv, char **envp)
     (void)argc;
     (void)argv;
     (void)envp;
+
+	clear_console();
 
     /*
      * When kernel_main() begins, interrupts are DISABLED.
