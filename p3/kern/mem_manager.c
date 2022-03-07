d3f9b613f34a3c9b70fbb50d17581a544c6898ef
Andre <cnasc.andre@gmaill.com>
2022-03-07 13:21:37 -0500

    Start implementation of vm and process manager

--- p3/kern/mem_manager.c
+++ p3/kern/mem_manager.c
@@ -8,13 +8,24 @@
 #include <stddef.h>
 #include <malloc.h>
 #include <elf/elf_410.h>
+#include <assert.h>
+#include <page.h>   /* PAGE_SIZE */
+#include <x86/cr.h> /* {get,set}_cr0 */
 
-#define PAGE_ENTRY_SIZE 64
-#define PAGE_TABLE_SIZE (1024 * PAGE_ENTRY_SIZE)
+#define PAGING_FLAG 0x80000000
 
-/* Page directory and page table coincidentally have
- * the same size. */
-#define PAGE_DIRECTORY_SIZE PAGE_TABLE_SIZE
+#define PAGE_DIRECTORY_INDEX 0xFFC00000
+#define PAGE_TABLE_INDEX 0x003FF000
+#define PAGE_OFFSET 0x00000FFF
+
+#define PAGE_DIRECTORY_SHIFT 22
+#define PAGE_TABLE_SHIFT 12
+
+/* Get page directory or page table index from a linear address. */
+#define PD_INDEX(addr) \
+    ((PAGE_DIRECTORY_INDEX & (addr)) >> PAGE_DIRECTORY_SHIFT)
+#define PT_INDEX(addr) \
+    ((PAGE_TABLE_INDEX & (addr)) >> PAGE_TABLE_SHIFT)
 
 /** lmm and malloc are responsible for managing kernel virtual
  *  memory. However, that virtual memory must still be associated
@@ -33,6 +44,7 @@
  *        the first phys frames are available. */
 static void *next_free_phys_frame;
 
+
 /** Initialize virtual memory */
 int
 vm_init( void )
@@ -40,23 +52,71 @@ vm_init( void )
 
     next_free_phys_frame = USER_MEM_START;
 
+    assert(next_free_phys_frame & (PAGE_SIZE - 1) == 0);
+
     return 0;
 }
 
-/** Allocate new pages in a given process' virtual memory. */
+/** Use this function to set protection bits. Should only be called
+ *  after data has been "transplanted" into process memory.
+ *
+ *  TODO: Don't assume, check to the extent possible
+ *  Assumes ptd has been initialized and allocated with same elf
+ *  header as is passed in here. */
 int
-vm_new_pages ( void *ptd_start, void *base, int len )
+set_protection_bits ( void *ptd, simple_elf_t *elf )
 {
-    return -1;
+
+}
+
+static uint32_t
+get_pd_index ( uint32_t linear_address ) {
+}
+
+static uint32_t
+get_pt_index ( uint32_t linear_address ) {
+}
+
+static void
+allocate_region( void *ptd, void *start, uint32_t len )
+{
+    // TODO: Affirm we have enough phys frames
+
+    uint32_t curr = (uint32_t)start;
+    uint32_t end = curr + len;
+
+    /* Allocate 1 frame at a time. */
+    while (curr < (uint32_t)start + len) {
+        /* Decompose curr into pd offset, pt offset. */
+        // TODO: Use these to write into PD and PT
+        // Also ask for new phys frame to put there
+        PD_INDEX(curr);
+        PT_INDEX(curr);
+
+        curr += PAGE_SIZE;
+    }
+
+    /* Zero out leftover bytes still in page */
+    /* FIXME: I don't think this does what it's supposed to */
+    memset((char *)start + len, 0, curr - end);
 }
 
 /** Allocate memory for new task at given page table directory.
+ *  Assumes page table directory is empty.
  *
+ *  When initializing memory regions specified in elf header,
+ *  zeroes out bytes after after their end but before the page
+ *  boundary.
+ *
+ *  TODO: Consider checking and returning on error
  *  TODO: Create a subroutine suitable for cloning. */
 int
-vm_new_task ( void *ptd_start, simple_elf_t *elf )
+vm_new_task ( void *ptd, simple_elf_t *elf )
 {
+    /* NOTE: We don't actually have to disable paging here. */
+
     /* Allocate phys frames and assign to virtual memory */
+
 
     /* Allocate rodata, text with read-only permissions */
 
@@ -67,6 +127,29 @@ vm_new_task ( void *ptd_start, simple_elf_t *elf )
     return -1;
 }
 
+void
+enable_paging( void )
+{
+	uint32_t current_cr0 = get_cr0();
+	set_cr0(current_cr0 | PAGING_FLAG);
+}
+void
+disable_paging( void )
+{
+	uint32_t current_cr0 = get_cr0();
+	set_cr0(current_cr0 & (~PAGING_FLAG));
+}
+
+
+/** Allocate new pages in a given process' virtual memory. */
+int
+vm_new_pages ( void *ptd_start, void *base, int len )
+{
+    return -1;
+}
+
+
+// FIXME: Having this might allow for poor code organization
 /** Translate logical address into physical address.
  *
  *  Note: This can be used by loader to "transplant"
--- p3/kern/process_manager.c
+++ p3/kern/process_manager.c
@@ -3,44 +3,169 @@
 #include <stdint.h>
 #include <stddef.h>
 #include <malloc.h>
+#include <elf/elf_410.h>
+#include <page.h> /* PAGE_SIZE */
+#include <string.h> /* memset */
 
-// TODO: Have this be defined in an internal .h file
-// and be shared with mem_manager.c
-#define PAGE_ENTRY_SIZE 64
-#define PAGE_TABLE_SIZE (1024 * PAGE_ENTRY_SIZE)
 
-/* Page directory and page table coincidentally have
- * the same size. */
-#define PAGE_DIRECTORY_SIZE PAGE_TABLE_SIZE
+// TODO: Have all these be redefined in an internal .h file
+#define WORD_SIZE 4
+#define PAGE_TABLE_ENTRIES (PAGE_SIZE / WORD_SIZE)
+#define PAGE_DIRECTORY_ENTRIES PAGE_TABLE_ENTRIES
+#define USER_MODE 0
+#define KERN_MODE 1
 
 typedef struct pcb pcb_t;
 typedef struct tcb tcb_t;
 
+/** @brief Task control block */
 struct pcb {
-    void *ptd_start; // Start of page table directory
+    void *ptd; // page table directory
     tcb_t *first_thread; // First thread in linked list
 
+    int pid;
 };
 
+/** @brief Thread control block */
+// TODO: Add necessary registers + program counter
 struct tcb {
     pcb_t *owning_task;
     tcb_t *next_thread; // Embeded linked list of threads from same process
 
+    int tid;
+    int mode; // USER_MODE or KERN_MODE, for bookkeeping only
+
+    /* Stack info. Needed for resuming execution.
+     * General purpose registers, program counter and
+     * data segment selectors are stored on stack pointed
+     * to by ss:esp. */
+    int ss;
+    int esp;
 };
 
-pcb_t pcb_list_start;
+pcb_t *pcb_list_start = NULL;
 
-/* Create new process (memory + pcb and tcb) */
+static int find_pcb( int pid, pcb_t **pcb );
+
+static int new_pcb( int pid, int tid );
+
 int
-new_task( simple_elf_t *elf )
+set_task_esp( int ss, int esp )
 {
-    // TODO: Affirm kernel_mode
-
-    /* TODO: Paging might be enabled, check for that and disable it. */
-
-    // TODO: Add to pcb_list, for now support 1 task (just set pcb_list_start)
-
-    /*-- Checkpoint 1 impl start --*/
-    pcb_list_start->ptd_start = malloc(
 }
 
+/*  Create new process
+ *
+ *  @arg pid Task id for new task
+ *  @arg tid Thread id for new thread
+ *  @arg elf Elf header for use in allocating new task's memory
+ *
+ *  @return 0 on success, negative value on failure.
+ * */
+int
+new_task( int pid, int tid, simple_elf_t *elf )
+{
+    // TODO: Think about preconditions for this.
+    // Paging fine, how about making it a critical section?
+
+    if (new_pcb(pid) < 0)
+        return -1;
+
+    if (new_tcb(pid, tid) < 0)
+        return -1;
+
+    /* Allocate VM */
+    pcb_t *pcb;
+    affirm(find_pcb(pid, &pcb) == 0);
+    vm_new_task(pcb->ptd, elf);
+
+    /* Enable paging */
+    vm_enable_paging();
+
+    return 0;
+}
+
+/** Looks for pcb with given pid.
+ *
+ *  @arg pid Task id to look for
+ *  @arg pcb Memory location where to store pcb, if found.
+ *
+ *  @return 0 on success, negative value on error. */
+static int
+find_pcb( int pid, pcb_t **pcb )
+{
+    // TODO: Actually implement the search
+    if (!pcb_list_start)
+        return -1;
+    *pcb = pcb_list_start;
+    return 0;
+}
+
+/* Initializes new pcb. */
+static int
+new_pcb( int pid, int tid )
+{
+    /* Ensure alignment of page table directory */
+    void *ptd = smemalign(PAGE_SIZE, PAGE_SIZE);
+    if (!ptd)
+        return -1;
+
+    assert(ptd & (PAGE_SIZE - 1) == 0);
+
+    pcb_t *new_pcb = malloc(sizeof(pcb_t));
+    if (!new_pcb) {
+        sfree(ptd, PAGE_SIZE);
+        return -1;
+    }
+
+    /* Ensure all entries are 0 and therefore not present */
+    memset(ptd, 0, PAGE_SIZE);
+
+    new_pcb->ptd = ptd;
+    new_pcb->pid = pid;
+    new_pcb->first_thread = NULL;
+
+    pcb_list_start = new_pcb;
+}
+
+int
+new_tcb( int pid, int tid )
+{
+    tcb_t *new_tcb = malloc(sizeof(tcb_t));
+    if (!new_tcb) {
+        return -1;
+    }
+
+    /* Set tcb/pcb values  */
+    new_tcb->owning_task = new_pcb;
+    new_tcb->next_thread = NULL;
+    new_tcb->tid = tid;
+    new_tcb->mode = USER_MODE;
+
+    pcb_t *owning_task;
+    if (find_pcb(pid, &owning_task) < 0) {
+        free(new_tcb);
+        return -1;
+    }
+    owning_task->first_thread = new_tcb;
+
+}
+
+/** Call to run task.
+ *  Should only ever be called once, and after a call to new_task.
+ *  The caller is supposed to install memory on the new task before
+ *  calling this function. Stack pointer should be appropriately set
+ *  if any arguments have been loaded on stack.
+ *
+ *  @arg tid Thread id of thread to run
+ *  @arg ss  Stack segment selector
+ *  @arg esp Stack pointer
+ *  @arg eip Task entry point
+ *
+ *  @return Doesn't return.
+ *  */
+void
+run_task( int tid, int ss, int esp, int eip )
+{
+
+}
