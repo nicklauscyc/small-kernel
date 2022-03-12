444ecc1b75187a3374b45ab835ac482597bd97d5
Nicklaus Choo <44658711+nicklauscyc@users.noreply.github.com>
2022-03-12 12:42:35 -0500

    created a task_manager_private.h that has structure definition, instead of the one in inc

--- p3/kern/inc/task_manager.h
+++ p3/kern/inc/task_manager.h
@@ -6,33 +6,11 @@
 
 #include <stdint.h> /* uint32_t */
 #include <elf_410.h> /* simple_elf_t */
+#include <task_manager_private.h> /* pcb_t tcb_t */
 
 typedef struct pcb pcb_t;
 typedef struct tcb tcb_t;
 
-/* TODO: Do we even want PCB and TCB to be available to others? */
-/** @brief Task control block */
-struct pcb {
-    void *ptd; // page table directory
-    tcb_t *first_thread; // First thread in linked list
-
-    int pid;
-
-    int prepared; // Whether this task's VM has been initialized
-};
-
-/** @brief Thread control block */
-struct tcb {
-    pcb_t *owning_task;
-    tcb_t *next_thread; // Embeded linked list of threads from same task
-
-    int tid;
-
-    /* Stack info. Needed for resuming execution.
-     * General purpose registers, program counter
-     * are stored on stack pointed to by esp. */
-    int esp;
-};
 
 int task_new( int pid, int tid, simple_elf_t *elf );
 int task_prepare( int pid );
---
+++ p3/kern/task_manager_private.h
@@ -0,0 +1,36 @@
+/** @brief Internal struct definitions not published in the interface
+ */
+
+#ifndef _TASK_MANAGER_PRIVATE_H_
+#define _TASK_MANAGER_PRIVATE_H_
+
+typedef struct pcb pcb_t;
+typedef struct tcb tcb_t;
+
+/* TODO: Do we even want PCB and TCB to be available to others? */
+/** @brief Task control block */
+struct pcb {
+    void *ptd; // page table directory
+    tcb_t *first_thread; // First thread in linked list
+
+    int pid;
+
+    int prepared; // Whether this task's VM has been initialized
+};
+
+/** @brief Thread control block */
+struct tcb {
+    pcb_t *owning_task;
+    tcb_t *next_thread; // Embeded linked list of threads from same task
+
+    int tid;
+
+    /* Stack info. Needed for resuming execution.
+     * General purpose registers, program counter
+     * are stored on stack pointed to by esp. */
+    int esp;
+};
+
+
+#endif /* _TASK_MANAGER_PRIVATE_H_ */
+
