2708f86f4704e8b9c72794a45c394d200a8dda58
Nicklaus Choo <44658711+nicklauscyc@users.noreply.github.com>
2022-03-14 20:44:04 -0400

    delete dead code comments

--- p3/kern/asm_interrupt_handler_template.h
+++ p3/kern/asm_interrupt_handler_template.h
@@ -17,12 +17,6 @@ call_ ## HANDLER_NAME ## :;\
 	popa; /* Restores all registers onto the stack */\
 	iret; /* Return to procedure before interrupt */
 
-/*
-//	mov $SEGSEL_KERNEL_CS, %ax;\
-//	mov %ax, %cs;\
-//	mov $SEGSEL_KERNEL_DS, %ax;\
-//	mov %ax, %ss;\
-//	*/
 #define CALL_W_RETVAL_HANDLER(HANDLER_NAME)\
 \
 .globl call_##HANDLER_NAME; /* create the asm function call_HANDLER_NAME */\
