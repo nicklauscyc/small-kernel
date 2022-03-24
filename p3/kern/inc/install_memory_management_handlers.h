ce5ff37b2475e2ad742815a4fd649365cd03a273
Nicklaus Choo <44658711+nicklauscyc@users.noreply.github.com>
2022-03-24 12:00:37 -0400

    fix some typos

--- p3/kern/inc/install_memory_management_handlers.h
+++ p3/kern/inc/install_memory_management_handlers.h
@@ -7,9 +7,10 @@
 #ifndef _INSTALL_MEMORY_MANAGEMENT_HANDLERS_H_
 #define _INSTALL_MEMORY_MANAGEMENT_HANDLERS_H_
 
-/** @brief Assembly wrapper for calling gettid() interrupt handler
+/** @brief Assembly wrapper for calling pagefault_handler() interrupt handler
  */
 void call_pagefault_handler(void);
 int install_pf_handler(int idt_entry, asm_wrapper_t *asm_wrapper);
+
 #endif /* _INSTALL_MEMORY_MANAGEMENT_HANDLERS_H_ */
 
