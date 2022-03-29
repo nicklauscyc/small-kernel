da2d613454ddcccb41ee52906aa227dd923c81f2
Nicklaus Choo <44658711+nicklauscyc@users.noreply.github.com>
2022-03-29 15:35:29 -0400

    task_manager_internal.h reinstated

--- p3/kern/inc/task_manager.h
+++ p3/kern/inc/task_manager.h
@@ -9,51 +9,8 @@
 #include <variable_queue.h>        /* Q_NEW_LINK */
 #include <task_manager_internal.h> /* pcb_t tcb_t */
 
-enum status { RUNNING, RUNNABLE, DESCHEDULED, BLOCKED, DEAD, UNINITIALIZED };
-typedef enum status status_t;
-
 typedef struct pcb pcb_t;
 typedef struct tcb tcb_t;
-
-/* pcb_queue definition */
-Q_NEW_HEAD(owned_threads_queue_t, tcb);
-
-/** @brief Task control block */
-struct pcb {
-    void *pd; // page directory
-	owned_threads_queue_t owned_threads; // list of owned threads
-	uint32_t num_threads;
-
-    pcb_t *next_task; // Embedded list of tasks TODO change to macro
-
-    uint32_t pid;
-
-    int prepared; // Whether this task's VM has been initialized
-};
-
-/** @brief Thread control block */
-struct tcb {
-	Q_NEW_LINK(tcb) scheduler_queue;
-	Q_NEW_LINK(tcb) tid2tcb_queue;
-	Q_NEW_LINK(tcb) owning_task_thread_list;
-
-
-    status_t status;
-
-    pcb_t *owning_task;
-    tcb_t *next_thread; // Embedded linked list of threads from same task
-
-    uint32_t tid;
-
-    /* Stack info. Needed for resuming execution.
-     * General purpose registers, program counter
-     * are stored on stack pointed to by esp. */
-    uint32_t *kernel_stack_hi; /* Highest _writable_ address in kernel stack */
-	                           /* that is stack aligned */
-    uint32_t *kernel_esp; // needed for context switch
-	uint32_t *kernel_stack_lo;
-};
-
 
 tcb_t *find_tcb( uint32_t tid );
 pcb_t *find_pcb( uint32_t pid );
