e6832b3f49f21ad8b9423e7272804ca18c3e9d74
Nicklaus Choo <44658711+nicklauscyc@users.noreply.github.com>
2022-04-07 11:20:31 -0400

    remove unecessary files and touch up exec helpers

--- p3/kern/lib_life_cycle/exec.c
+++ p3/kern/lib_life_cycle/exec.c
@@ -1,10 +1,10 @@
 /** @file exec.c
- *  @brief Contains exec interrupt handler and helper functions for
- *         installation
+ *	@brief Contains exec interrupt handler and helper functions for
+ *		   installation
  *
- *  @author Nicklaus Choo (nchoo)
+ *	@author Nicklaus Choo (nchoo)
  */
-#include <elf_410.h>    /* simple_elf_t, elf_load_helper */
+#include <elf_410.h>	/* simple_elf_t, elf_load_helper */
 #include <seg.h>	/* SEGSEL_... */
 #include <eflags.h>	/* get_eflags*/
 #include <string.h> /* strlen(), memcpy(), memset() */
@@ -22,14 +22,14 @@
 #include <x86/interrupt_defines.h> /* INT_CTL_PORT, INT_ACK_CURRENT */
 #include <x86/asm.h> /* outb() */
 
-#define EXEC_STR_LEN 256
-#define NUM_EXEC_ARGS 16
+#define USER_STR_LEN 256
+#define NUM_USER_ARGS 16
 
 /** @brief Prints arguments passed to exec() when log level is DEBUG
  *
- *  @param execname Executable name
- *  @param argvec Argument vector
- *  @return Void.
+ *	@param execname Executable name
+ *	@param argvec Argument vector
+ *	@return Void.
  */
 static void
 log_exec_args( char *execname, char **argvec)
@@ -44,31 +44,111 @@ log_exec_args( char *execname, char **argvec)
 }
 
 /** @brief Checks that the address of every character in the string is a valid
- *         address
+ *		   address
  *
- *  @param s String to be checked
+ *	The maximum permitted string length is USER_STR_LEN, including '\0'
+ *	terminating character. Therefore the longest possible user string will
+ *	have at most USER_STR_LEN - 1 non-NULL characters.
+ *
+ *	This does not check for the existence of a user executable with this
+ *	name. That is done when we try to fill in the ELF header.
+ *
+ *	@param s String to be checked
+ *	@return 1 if valid user string, 0 otherwise
  */
-int is_valid_user_string( char *s )
+int
+is_valid_user_string( char *s )
 {
+	/* Check address of every character in s */
+	int i;
+	for (i = 0; i < USER_STR_LEN; ++i) {
+
+		if (!is_user_pointer_valid(s + i)) {
+			log_warn("invalid address %p at index %d of user string %s",
+					 s + i, i, s);
+			return 0;
+
+		} else {
+
+			/* String has ended within USER_STR_LEN */
+			if (s[i] == '\0') {
+				break;
+			}
+		}
+	}
+	/* Check length of s */
+	if (i == USER_STR_LEN) {
+		log_warn("user string of length >= USER_STR_LEN");
+		return 0;
+	}
+	return 1;
+}
+
+/** @brief Checks address of every char * in argvec, argvec has max length
+ *		   of < NUM_USER_ARGS
+ *
+ *	@param execname Executable name
+ *	@param argvec Argument vector
+ *	@return Number of user args if valid argvec, 0 otherwise
+ */
+int
+is_valid_user_argvec( char *execname,  char **argvec )
+{
+	/* Check address of every char * in argvec */
+	int i;
+	for (i = 0; i < NUM_USER_ARGS; ++i) {
+
+		/* Invalid char ** */
+		if (!is_user_pointer_valid(argvec + i)) {
+			log_warn("invalid address %p at index %d of argvec", argvec + i, i);
+			return 0;
+
+		/* Valid char **, so check if char * is valid */
+		} else {
+
+			/* String has ended within NUM_USER_ARGS */
+			if (argvec[i] == NULL) {
+				break;
+			}
+			/* Check if valid string */
+			if (!is_valid_user_string(argvec[i])) {
+				log_warn("invalid address user string %s at index %d of argvec",
+						 argvec[i], i);
+				return 0;
+			}
+		}
+	}
+	/* Check length of arg_vec */
+	if (i == NUM_USER_ARGS) {
+		log_warn("argvec has length >= NUM_USER_ARGS");
+		return 0;
+	}
+	/* Check if argvec[0] == execname */
+	if (strcmp(argvec[0],execname) != 0) {
+		log_warn("argvec[0]:%s not equal to execname:%s", argvec[0], execname);
+		return 0;
+	}
+	return i;
+}
 
 /** @brief Executes execname with arguments in argvec
  *
- *  argvec can have a maximum of 16 elements, which is admittedly an arbitrary
- *  power of 2, but most executable invocations usually use less than 16
- *  parameters.
+ *	argvec can have a maximum of 16 elements, which is admittedly an arbitrary
+ *	power of 2, but most executable invocations usually use less than 16
+ *	parameters.
  *
- *  execname and each of the string arguments can have < EXEC_STR_LEN
- *  characters, which is a gain another reasonable limit for string length.
+ *	execname and each of the string arguments can have < USER_STR_LEN
+ *	characters, which is a gain another reasonable limit for string length.
  *
- *  @param execname Executable name
- *  @param argvec String array of arguments for executable execname
- *  @return Does not return on success, -1 on error
+ *	@param execname Executable name
+ *	@param argvec String array of arguments for executable execname
+ *	@return Does not return on success, -1 on error
  */
 int
 exec( char *execname, char **argvec )
 {
 	/* Acknowledge interrupt immediately */
-    outb(INT_CTL_PORT, INT_ACK_CURRENT);
+	outb(INT_CTL_PORT, INT_ACK_CURRENT);
 
 	/* Only allow exec of task that has 1 thread */
 	uint32_t tid = get_running_tid();
@@ -82,72 +162,44 @@ exec( char *execname, char **argvec )
 	}
 	assert(num_threads == 1);
 
-	if (!execname) {
+	/* Validate execname */
+	if (!is_valid_user_string(execname)) {
 		return -1;
 	}
-	if (!argvec) {
+	/* Validate argvec */
+	int argc = 0;
+	if (!(argc = is_valid_user_argvec(execname, argvec))) {
 		return -1;
 	}
-
-
-
-
-
-
-
-	// TODO check if argvec[0] == execname
-	// TODO limit number of args
-	// TODO limit length of each arg
 	// TODO ensure no software exception handler registered
 	log_exec_args(execname, argvec);
 
-	/* Count number of arguments */
-	int argc = 0;
-	int i = 0;
-	while (argvec[i]) {
-		if (strlen(argvec[i]) >= EXEC_STR_LEN) {
-			return -1;
-		}
-		++argc;
-		++i;
-	}
-	(void) argc;
-	if (argc > NUM_EXEC_ARGS) {
-		return -1;
-	}
-	/* This is on the kernel stack and is unaffected by page directory */
-	char fname[EXEC_STR_LEN];
-	memset(fname, 0, EXEC_STR_LEN);
-	memcpy(fname, execname, strlen(execname));
+	/* Transfer execname to kernel stack so unaffected by page directory */
+	char kern_stack_execname[USER_STR_LEN];
+	memset(kern_stack_execname, 0, USER_STR_LEN);
+	memcpy(kern_stack_execname, execname, strlen(execname));
 
-	/* TODO argvec also has to be transferred to kernel memory */
-	char kern_stack_args[NUM_EXEC_ARGS * EXEC_STR_LEN];
-	char *kern_stack_argvec[NUM_EXEC_ARGS];
+	/* char array to store each argvec string on kernel stack */
+	char kern_stack_args[NUM_USER_ARGS * USER_STR_LEN];
+	memset(kern_stack_args, 0, NUM_USER_ARGS * USER_STR_LEN);
 
-	memset(kern_stack_args, 0, NUM_EXEC_ARGS * EXEC_STR_LEN);
-	memset(kern_stack_argvec, 0, NUM_EXEC_ARGS * EXEC_STR_LEN);
+	/* char * array for argvec on kernel stack */
+	char *kern_stack_argvec[NUM_USER_ARGS];
+	memset(kern_stack_argvec, 0, NUM_USER_ARGS);
 
+	/* Transfer argvec to kernel stack so unaffected by page directory */
 	int offset = 0;
 	for (int i = 0; argvec[i]; ++i) {
 		char *arg = argvec[i];
 		memcpy(kern_stack_args + offset, arg, strlen(arg));
 		kern_stack_argvec[i] = kern_stack_args + offset;
-		offset += EXEC_STR_LEN;
+		offset += USER_STR_LEN;
 	}
-
-	log("fname:'%s'", fname);
-	i = 0;
-	while (kern_stack_argvec[i]) {
-		log("kern_stack_argvec[%d]:'%s'", i, kern_stack_argvec[i]);
-		++i;
-	}
-
-
-	if (execute_user_program(fname, argc, kern_stack_argvec, 1) < 0) {
+	/* Execute */
+	if (execute_user_program(kern_stack_execname, argc, kern_stack_argvec, 1)
+		< 0) {
 		return -1;
 	}
-
-
+	panic("exec() should not come here!");
 	return -1;
 }
-
